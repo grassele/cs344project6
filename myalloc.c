ea45b01196dfcb4f0d229f2c2f4eb8738b326ca9
grassele <91402260+grassele@users.noreply.github.com>
2023-02-20 21:33:01 -0800

    part 1 working, removed comments

--- myalloc.c
+++ myalloc.c
@@ -1,38 +1,29 @@
 #include <stdio.h>
 #include <sys/mman.h>
 
-
-// clang -Wall -Wextra -o myalloc myalloc.c
-
-
-// macros
-#define ALIGNMENT 16   // Must be power of 2
+#define ALIGNMENT 16
 #define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
 #define PADDED_SIZE(x) ((x) + GET_PAD(x))
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
 
 
-// declarations
 struct block {
     struct block *next;
     int size;     // Bytes
     int in_use;   // Boolean
 };
+
 void *myalloc(int size);
+
 void print_data(void);
+
 // void myfree(void *p);
 
 
-// global variable
 struct block *head = NULL;
-// size_t available_space = 1024; // added
 
 
-
-int main(void) {    
-    
-}
-
+int main(void) {}
 
 
 void *myalloc(int size) {
@@ -40,24 +31,23 @@ void *myalloc(int size) {
             head = mmap(NULL, 1024, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
             head->next = NULL;
             head->size = 1024 - PADDED_SIZE(sizeof(struct block));
-            // available_space = head->size; // added
             head->in_use = 0;
     }
-    
+
     struct block *n = head;
     size_t padded_block_size = PADDED_SIZE(sizeof(struct block));
+
     while (n != NULL) {
-        // struct block *prev = head; - for modification of "next" field
-        if (n->size >= PADDED_SIZE(size) && !n->in_use) { // + block size?
-            // n->size = PADDED_SIZE(size);
+        if (n->size >= PADDED_SIZE(size) && !n->in_use) {
             n->in_use = 1;
             return PTR_OFFSET(n, padded_block_size);
         }
         n = n->next;
     }
-    return NULL;
-    
+
+    return NULL; 
 }
+
 
 void print_data(void) {
     struct block *b = head;
@@ -74,11 +64,11 @@ void print_data(void) {
         if (b->next != NULL) {
             printf(" -> ");
         }
-
         b = b->next;
     }
 
     printf("\n");
 }
+
 
 // void myfree(void *p);
