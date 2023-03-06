61f4134e4763e36113025193ed80dc1b43841732
grassele <91402260+grassele@users.noreply.github.com>
2023-03-05 16:28:25 -0800

    fixed stupid overcomplication mistake, merging free blocks works with all examples

--- myalloc.c
+++ myalloc.c
@@ -23,11 +23,17 @@ int padded_block_size = PADDED_SIZE(sizeof(struct block));
 
 
 int main(void) {
-    void *p;
+    void *p, *q, *r, *s;
 
     p = myalloc(10); print_data();
+    q = myalloc(20); print_data();
+    r = myalloc(30); print_data();
+    s = myalloc(40); print_data();
 
+    myfree(q); print_data();
     myfree(p); print_data();
+    myfree(s); print_data();
+    myfree(r); print_data();
 }
 
 
@@ -80,7 +86,9 @@ void myfree(void *p) {
             cur->size += padded_block_size + (cur->next)->size;
             cur->next = (cur->next)->next;
         }
-        cur = cur->next;
+        else {
+            cur = cur->next;
+        }
     }
 }
 
